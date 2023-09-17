6cabd1cd5c0bce3b1bb9884fb90a101305e7310d
Mitong Dapal <mitongdapal@gmail.com>
2023-09-17 13:51:33 +0100

    Update App.tsx

--- Frontend/wallet-integration-rainbowkit/src/App.tsx
+++ Frontend/wallet-integration-rainbowkit/src/App.tsx
@@ -105,7 +105,7 @@ export function App() {
   return (
     <section className={"container"}>
       <nav>
-        RainbowKit
+        Colour Board - Contract Deployed to Sepolia
         <div className={"nav-connect-button"}>
           <ConnectButton />
         </div>
